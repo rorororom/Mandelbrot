9c8ba476a5e3c0c6cfb0ec24f3ddb9b1d2dde63a
rorororom <mariia.romashkina.mih@gmail.com>
2024-03-28 14:07:15 +0300

    redone redone redone

--- version1.cpp
+++ version1.cpp
@@ -13,29 +13,29 @@ const float RADIUS         = 100.0f;
 #define TIME_MEASURE
 // #define CNT_FPS
 
-void writeFPS(sf::RenderWindow& window, sf::Text& fpsText, sf::Clock& gameClock, int& frames, int& cntForFps, unsigned long long& all_fps) {
-    frames++;
-    sf::Time elapsed = gameClock.getElapsedTime();
+void writeFPS(sf::RenderWindow* window, sf::Text* fpsText, sf::Clock* gameClock, int* frames, int* cntForFps, unsigned long long* all_fps) {
+    (*frames)++;
+    sf::Time elapsed = gameClock->getElapsedTime();
     if (elapsed.asSeconds() >= 1.0f) {
-        float fps = frames / gameClock.getElapsedTime().asSeconds();
+        float fps = (*frames) / gameClock->getElapsedTime().asSeconds();
 
         #ifndef CNT_FPS
-        all_fps += fps;
+        *all_fps += fps;
         #endif
 
         #ifdef TIME_MEASURE
-        if (cntForFps == LIMIT) {
-            printf("FPS: %f\n", all_fps / LIMIT);
+        if (*cntForFps == LIMIT) {
+            printf("FPS: %f\n", *all_fps / LIMIT);
         }
         #endif
         std::string fpsStr = "FPS: " + std::to_string(static_cast<int>(fps));
 
-        fpsText.setString(fpsStr);
-        frames = 0;
-        gameClock.restart();
+        fpsText->setString(fpsStr);
+        (*frames) = 0;
+        gameClock->restart();
     }
 
-    window.draw(fpsText);
+    window->draw(*fpsText);
 }
 
 inline int mandelbrot(float x0, float y0) {
@@ -56,30 +56,30 @@ inline int mandelbrot(float x0, float y0) {
         return iteration;
 }
 
-inline void handleKeyPress(sf::RenderWindow& window, float& xC, float& yC, float& zoom) {
+inline void handleKeyPress(sf::RenderWindow* window, float* xC, float* yC, float* zoom) {
     sf::Event event;
-    while (window.pollEvent(event)) {
+    while (window->pollEvent(event)) {
         if (event.type == sf::Event::Closed)
-            window.close();
+            window->close();
         else if (event.type == sf::Event::KeyPressed) {
             switch (event.key.code) {
                 case sf::Keyboard::Right:
-                    xC += MOVE_FACTOR; // Move image right
+                    (*xC) += MOVE_FACTOR; // Move image right
                     break;
                 case sf::Keyboard::Left:
-                    xC -= MOVE_FACTOR; // Move image left
+                    (*xC) -= MOVE_FACTOR; // Move image left
                     break;
                 case sf::Keyboard::Up:
-                    yC -= MOVE_FACTOR; // Move image up
+                    (*yC) -= MOVE_FACTOR; // Move image up
                     break;
                 case sf::Keyboard::Down:
-                    yC += MOVE_FACTOR; // Move image down
+                    (*yC) += MOVE_FACTOR; // Move image down
                     break;
                 case sf::Keyboard::Equal:
-                    zoom /= ZOOM_FACTOR; // Zoom in
+                    (*zoom) /= ZOOM_FACTOR; // Zoom in
                     break;
                 case sf::Keyboard::Dash:
-                    zoom *= ZOOM_FACTOR; // Zoom out
+                    (*zoom) *= ZOOM_FACTOR; // Zoom out
                     break;
                 default:
                     break;
@@ -88,25 +88,25 @@ inline void handleKeyPress(sf::RenderWindow& window, float& xC, float& yC, float
     }
 }
 
-inline void initialize(sf::RenderWindow& window, sf::Image& image, sf::Texture& texture, sf::Sprite& sprite, sf::Text& fpsText, sf::Font& font) {
-    window.create(sf::VideoMode(WIDTH, HEIGHT), "Mandelbrot Set");
+inline void initialize(sf::RenderWindow* window, sf::Image* image, sf::Texture* texture, sf::Sprite* sprite, sf::Text* fpsText, sf::Font* font) {
+    window->create(sf::VideoMode(WIDTH, HEIGHT), "Mandelbrot Set");
 
-    image.create(WIDTH, HEIGHT, sf::Color::Black);
-    texture.create(WIDTH, HEIGHT);
-    sprite.setTexture(texture);
+    image->create(WIDTH, HEIGHT, sf::Color::Black);
+    texture->create(WIDTH, HEIGHT);
+    sprite->setTexture(*texture);
 
-    font.loadFromFile("arial.ttf");
-    fpsText.setFont(font);
-    fpsText.setCharacterSize(20);
-    fpsText.setFillColor(sf::Color::Red);
-    fpsText.setPosition(10, 10);
+    font->loadFromFile("arial.ttf");
+    fpsText->setFont(*font);
+    fpsText->setCharacterSize(20);
+    fpsText->setFillColor(sf::Color::Red);
+    fpsText->setPosition(10, 10);
 }
 
-inline void processEvents(sf::RenderWindow& window, float& xC, float& yC, float& zoom, sf::Image& image, sf::Texture& texture, sf::Sprite& sprite, sf::Text& fpsText, sf::Clock& gameClock, int& frames) {
+inline void processEvents(sf::RenderWindow* window, float* xC, float* yC, float* zoom, sf::Image* image, sf::Texture* texture, sf::Sprite* sprite, sf::Text* fpsText, sf::Clock* gameClock, int* frames) {
     int cntForTick = 0, cntForFps = 0;
     unsigned long long all_time = 0, all_fps = 0;
 
-    while (window.isOpen()) {
+    while (window->isOpen()) {
         handleKeyPress(window, xC, yC, zoom);
 
         #ifdef TIME_MEASURE
@@ -115,25 +115,24 @@ inline void processEvents(sf::RenderWindow& window, float& xC, float& yC, float&
 
         for (int y = 0; y < HEIGHT; y++) {
             for (int x = 0; x < WIDTH; x++) {
-                float xx = (float)x / WIDTH * 3.5f * zoom - 2.5f + xC;
-                float yy = (float)y / HEIGHT * 2.0f * zoom - 1.0f + yC;
+                float xx = (float)x / WIDTH * 3.5f * (*zoom) - 2.5f + (*xC);
+                float yy = (float)y / HEIGHT * 2.0f * (*zoom) - 1.0f + (*yC);
                 volatile int color = mandelbrot(xx, yy);
 
                 #ifndef TIME_MEASURE
                 sf::Color sfColor((color * 6) % 256, 0, (color * 10) % 256);
-                image.setPixel(x, y, sfColor);
+                image->setPixel(x, y, sfColor);
                 #endif
             }
         }
 
         #ifdef TIME_MEASURE
         unsigned long long end = __rdtsc();
-        unsigned long long elapsedTime = end - start;
+        all_time += end - start;
         cntForTick++;
         #endif
 
         cntForFps++;
-        all_time += elapsedTime;
 
         #ifdef TIME_MEASURE
         if (cntForTick == LIMIT) {
@@ -141,13 +140,13 @@ inline void processEvents(sf::RenderWindow& window, float& xC, float& yC, float&
         }
         #endif
 
-        texture.update(image);
-        window.clear();
-        window.draw(sprite);
+        texture->update(*image);
+        window->clear();
+        window->draw(*sprite);
 
-        writeFPS(window, fpsText, gameClock, frames, cntForFps, all_fps);
+        writeFPS(window, fpsText, gameClock, frames, &cntForFps, &all_fps);
 
-        window.display();
+        window->display();
     }
 }
 
@@ -159,14 +158,15 @@ int main() {
     sf::Text         fpsText;
     sf::Font         font;
 
-    initialize(window, image, texture, sprite, fpsText, font);
+    initialize(&window, &image, &texture, &sprite, &fpsText, &font);
 
     sf::Clock gameClock;
     int frames = 0;
 
     float xC = 0.f, yC = 0.f, zoom = 1.0f;
 
-    processEvents(window, xC, yC, zoom, image, texture, sprite, fpsText, gameClock, frames);
+    processEvents(&window, &xC, &yC, &zoom, &image, &texture, &sprite, &fpsText, &gameClock, &frames);
 
     return 0;
 }
+
--- version4.cpp
+++ version4.cpp
@@ -2,7 +2,6 @@
 #include <cmath>
 #include <immintrin.h>
 #include <string.h>
-#include <stdio.h>
 
 const int    WIDTH          = 800;
 const int    HEIGHT         = 600;
@@ -14,55 +13,55 @@ const int    LIMIT          = 100.0;
 
 #define TIME_MEASURE
 
-void writeFPS(sf::RenderWindow& window, sf::Text& fpsText, sf::Clock& gameClock, int& frames, int& cntForFps, unsigned long long& all_fps) {
-    frames++;
-    sf::Time elapsed = gameClock.getElapsedTime();
+void writeFPS(sf::RenderWindow* window, sf::Text* fpsText, sf::Clock* gameClock, int* frames, int* cntForFps, unsigned long long* all_fps) {
+    (*frames)++;
+    sf::Time elapsed = gameClock->getElapsedTime();
     if (elapsed.asSeconds() >= 1.0f) {
-        float fps = frames / gameClock.getElapsedTime().asSeconds();
+        float fps = (*frames) / gameClock->getElapsedTime().asSeconds();
 
         #ifndef CNT_FPS
-        all_fps += fps;
+        *all_fps += fps;
         #endif
 
         #ifdef TIME_MEASURE
-        if (cntForFps == LIMIT) {
-            printf("FPS: %f\n", all_fps / LIMIT);
+        if (*cntForFps == LIMIT) {
+            printf("FPS: %f\n", *all_fps / LIMIT);
         }
         #endif
         std::string fpsStr = "FPS: " + std::to_string(static_cast<int>(fps));
 
-        fpsText.setString(fpsStr);
-        frames = 0;
-        gameClock.restart();
+        fpsText->setString(fpsStr);
+        (*frames) = 0;
+        gameClock->restart();
     }
 
-    window.draw(fpsText);
+    window->draw(*fpsText);
 }
 
-inline void handleKeyPress(sf::RenderWindow& window, float& xC, float& yC, float& zoom) {
+inline void handleKeyPress(sf::RenderWindow* window, float* xC, float* yC, float* zoom) {
     sf::Event event;
-    while (window.pollEvent(event)) {
+    while (window->pollEvent(event)) {
         if (event.type == sf::Event::Closed)
-            window.close();
+            window->close();
         else if (event.type == sf::Event::KeyPressed) {
             switch (event.key.code) {
                 case sf::Keyboard::Right:
-                    xC += MOVE_FACTOR; // Move image right
+                    (*xC) += MOVE_FACTOR; // Move image right
                     break;
                 case sf::Keyboard::Left:
-                    xC -= MOVE_FACTOR; // Move image left
+                    (*xC) -= MOVE_FACTOR; // Move image left
                     break;
                 case sf::Keyboard::Up:
-                    yC -= MOVE_FACTOR; // Move image up
+                    (*yC) -= MOVE_FACTOR; // Move image up
                     break;
                 case sf::Keyboard::Down:
-                    yC += MOVE_FACTOR; // Move image down
+                    (*yC) += MOVE_FACTOR; // Move image down
                     break;
                 case sf::Keyboard::Equal:
-                    zoom /= ZOOM_FACTOR; // Zoom in
+                    (*zoom) /= ZOOM_FACTOR; // Zoom in
                     break;
                 case sf::Keyboard::Dash:
-                    zoom *= ZOOM_FACTOR; // Zoom out
+                    (*zoom) *= ZOOM_FACTOR; // Zoom out
                     break;
                 default:
                     break;
@@ -99,11 +98,11 @@ inline void mandelbrot(const __m128 X0, const __m128 Y0, volatile __m128& color)
     }
 }
 
-inline void processEvents(sf::RenderWindow& window, float& xC, float& yC, float& zoom, sf::Image& image, sf::Texture& texture, sf::Sprite& sprite, sf::Text& fpsText, sf::Clock& gameClock, int& frames) {
+inline void processEvents(sf::RenderWindow* window, float* xC, float* yC, float* zoom, sf::Image* image, sf::Texture* texture, sf::Sprite* sprite, sf::Text* fpsText, sf::Clock* gameClock, int* frames) {
     int cntForTick = 0, cntForFps = 0;
     unsigned long long all_time = 0, all_fps = 0;
 
-    while (window.isOpen()) {
+    while (window->isOpen()) {
         handleKeyPress(window, xC, yC, zoom);
 
         #ifdef TIME_MEASURE
@@ -112,11 +111,11 @@ inline void processEvents(sf::RenderWindow& window, float& xC, float& yC, float&
 
         for (int y = 0; y < HEIGHT; y++) {
             for (int x = 0; x < WIDTH; x += 4) {
-                __m128 X0 = _mm_set_ps((float) (x + 3) / WIDTH * 3.5f * zoom - 2.5f + xC,
-                                        (float)(x + 2) / WIDTH * 3.5f * zoom - 2.5f + xC,
-                                        (float)(x + 1) / WIDTH * 3.5f * zoom - 2.5f + xC,
-                                        (float)(x + 0) / WIDTH * 3.5f * zoom - 2.5f + xC);
-                __m128 Y0 = _mm_set_ps1((float)y / HEIGHT * 2.0f * zoom - 1.0f + yC);
+                __m128 X0 = _mm_set_ps((float) (x + 3) / WIDTH * 3.5f * (*zoom) - 2.5f + (*xC),
+                                        (float)(x + 2) / WIDTH * 3.5f * (*zoom) - 2.5f + (*xC),
+                                        (float)(x + 1) / WIDTH * 3.5f * (*zoom) - 2.5f + (*xC),
+                                        (float)(x + 0) / WIDTH * 3.5f * (*zoom) - 2.5f + (*xC));
+                __m128 Y0 = _mm_set_ps1((float)y / HEIGHT * 2.0f * (*zoom) - 1.0f + (*yC));
 
                 volatile __m128 color = _mm_set_ps(0, 0, 0, 0);
                 mandelbrot(X0, Y0, color);
@@ -125,7 +124,7 @@ inline void processEvents(sf::RenderWindow& window, float& xC, float& yC, float&
                 int* color_int = (int*)(&color);
                 for (int i = 0; i < 4; i++) {
                     sf::Color sfColor((color_int[i] * 6) % 256, 0, (color_int[i] * 10) % 256);
-                    image.setPixel(x + i, y, sfColor);
+                    image->setPixel(x + i, y, sfColor);
                 }
                 #endif
             }
@@ -146,28 +145,28 @@ inline void processEvents(sf::RenderWindow& window, float& xC, float& yC, float&
         }
         #endif
 
-        texture.update(image);
-        window.clear();
-        window.draw(sprite);
+        texture->update(*image);
+        window->clear();
+        window->draw(*sprite);
 
-        writeFPS(window, fpsText, gameClock, frames, cntForFps, all_fps);
+        writeFPS(window, fpsText, gameClock, frames, &cntForFps, &all_fps);
 
-        window.display();
+        window->display();
     }
 }
 
-inline void initialize(sf::RenderWindow& window, sf::Image& image, sf::Texture& texture, sf::Sprite& sprite, sf::Text& fpsText, sf::Font& font) {
-    window.create(sf::VideoMode(WIDTH, HEIGHT), "Mandelbrot Set");
+inline void initialize(sf::RenderWindow* window, sf::Image* image, sf::Texture* texture, sf::Sprite* sprite, sf::Text* fpsText, sf::Font* font) {
+    window->create(sf::VideoMode(WIDTH, HEIGHT), "Mandelbrot Set");
 
-    image.create(WIDTH, HEIGHT, sf::Color::Black);
-    texture.create(WIDTH, HEIGHT);
-    sprite.setTexture(texture);
+    image->create(WIDTH, HEIGHT, sf::Color::Black);
+    texture->create(WIDTH, HEIGHT);
+    sprite->setTexture(*texture);
 
-    font.loadFromFile("arial.ttf");
-    fpsText.setFont(font);
-    fpsText.setCharacterSize(20);
-    fpsText.setFillColor(sf::Color::Red);
-    fpsText.setPosition(10, 10);
+    font->loadFromFile("arial.ttf");
+    fpsText->setFont(*font);
+    fpsText->setCharacterSize(20);
+    fpsText->setFillColor(sf::Color::Red);
+    fpsText->setPosition(10, 10);
 }
 
 int main() {
@@ -178,14 +177,14 @@ int main() {
     sf::Text         fpsText;
     sf::Font         font;
 
-    initialize(window, image, texture, sprite, fpsText, font);
+    initialize(&window, &image, &texture, &sprite, &fpsText, &font);
 
     sf::Clock gameClock;
     int frames = 0;
 
     float xC = 0.f, yC = 0.f, zoom = 1.0f;
 
-    processEvents(window, xC, yC, zoom, image, texture, sprite, fpsText, gameClock, frames);
+    processEvents(&window, &xC, &yC, &zoom, &image, &texture, &sprite, &fpsText, &gameClock, &frames);
 
     return 0;
 }
